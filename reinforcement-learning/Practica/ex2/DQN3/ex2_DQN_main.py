712c197a43f289c17ee8ba88129865fde19916f3
Jordi Puig <jordi@elabogado.com>
2023-01-04 21:55:21 +0100

    Update ex2_DQN_main.py

--- reinforcement-learning/Practica/ex2/DQN3/ex2_DQN_main.py
+++ reinforcement-learning/Practica/ex2/DQN3/ex2_DQN_main.py
@@ -4,6 +4,13 @@ from ex2_DQN_Agent import Agent
 import numpy as np
 import torch as T
 
+BUFFER_SIZE = 100000  # Màxima capacitat del buffer
+BATCH_SIZE = 64       # Conjunt a agafar del buffer per a la xarxa neuronal
+GAMMA = 0.99          # Valor gamma de l'equació de Bellman
+TAU = 1e-3            # Valor de tau per a soft update del target network
+LEARNING_RATE = 5e-4  # Velocitat d'aprenentatge
+DNN_UPD = 4           # Freqüència d'actualització de la xarxa neuronal
+
 # initialize environment
 env = gym.make('LunarLander-v2')
 env.seed(0)
@@ -11,8 +18,7 @@ print('State shape: ', env.observation_space.shape)
 print('Number of actions: ', env.action_space.n)
 
 # initialize agent
-agent = Agent(state_size=8, action_size=4, seed=0)
-
+agent = Agent(env, seed=0, learning_rate=LEARNING_RATE, gamma=GAMMA, tau=TAU, buffer_size=BUFFER_SIZE, batch_size=BATCH_SIZE, dnn_upd=DNN_UPD)
 
 def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
     """Deep Q-Learning.
@@ -46,8 +52,7 @@ def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
         if i_episode % 100 == 0:
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
         if np.mean(scores_window) >= 200.0:
-            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100,
-                                                                                         np.mean(scores_window)))
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode - 100, np.mean(scores_window)))
             T.save(agent.qnetwork_local.state_dict(), 'data.pth')
             break
     return scores        
