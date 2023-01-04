e4bb6af573a7e0d63566daf87306b7ef864401b6
Jordi Puig <jordi@elabogado.com>
2023-01-04 22:32:21 +0100

    Update ex2_DQN_main.py

--- reinforcement-learning/Practica/ex2/DQN3/ex2_DQN_main.py
+++ reinforcement-learning/Practica/ex2/DQN3/ex2_DQN_main.py
@@ -8,14 +8,9 @@ TAU = 1e-3            # Valor de tau per a soft update del target network
 LEARNING_RATE = 5e-4  # Velocitat d'aprenentatge
 DNN_UPD = 4           # Freqüència d'actualització de la xarxa neuronal
 
-# initialize environment
+# inicialització de l'entorn de gym
 env = gym.make('LunarLander-v2')
-env.seed(0)
-print('State shape: ', env.observation_space.shape)
-print('Number of actions: ', env.action_space.n)
-
-# initialize agent
+# inicialització de l'agent amb els paràmetres de l'exercici
 agent = Agent(env, seed=0, learning_rate=LEARNING_RATE, gamma=GAMMA, tau=TAU, buffer_size=BUFFER_SIZE, batch_size=BATCH_SIZE, dnn_upd=DNN_UPD)
-    
 # run the training session
 scores = agent.train()
